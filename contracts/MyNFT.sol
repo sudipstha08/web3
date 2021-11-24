f0a471a9a585839f37f4866901a7cc8222a2835f
Sudip Shrestha <sudipshrestha@Sudips-MacBook-Air.local>
2021-11-24 22:29:47 +0545

    init

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,31 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+// https://ethereum.org/en/developers/tutorials/how-to-write-and-deploy-an-nft/
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// provides counters that can only be incremented or decremented by one. 
+import "@openzeppelin/contracts/utils/Counters.sol";
+//
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() public ERC721("MyNFT", "NFT") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+    // A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts, 
+    // so MyNFT here is a factory for instances of our NFT contract.
+  const MyNFT = await ethers.getContractFactory("MyNFT")
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy()
+  console.log("Contract deployed to address:", myNFT.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
